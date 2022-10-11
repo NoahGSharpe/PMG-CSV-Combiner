4a4e4737fa12ee1361bc62f2fee4bab7517c40ce
Noah Gorgevski-Sharpe <ngsharpe13@gmail.com>
2022-10-11 16:32:58 -0400

    Create header line before loop

--- Combine_CSVs.py
+++ Combine_CSVs.py
@@ -2,15 +2,17 @@ import sys
 
 
 def main():
-    needHeader = True
+    if len(sys.argv) < 2:
+        return
+
+    with open(sys.argv[1]) as file:
+        print(file.readline()[:-1] + ",\"filename\"\n")
+
     for i in range(1, len(sys.argv)):
         path = sys.argv[i]
         filename = path[path.rfind('/')+1:]
         with open(path) as file:
             for j, line in enumerate(file):
-                if needHeader:
-                    print(line[:-1] + ",\"filename\"\n")
-                    needHeader = False
                 if j == 0 or line == '\n':
                     continue
                 print(line[:-1] + f",\"{filename}\"\n")

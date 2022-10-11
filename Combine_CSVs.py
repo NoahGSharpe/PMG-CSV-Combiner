2db89894e1db4ace1370bfd5d913a46e4df0a444
Noah Gorgevski-Sharpe <ngsharpe13@gmail.com>
2022-10-11 16:05:48 -0400

    Rough Draft Working

--- Combine_CSVs.py
+++ Combine_CSVs.py
@@ -2,7 +2,18 @@ import sys
 
 
 def main():
+    needHeader = True
+    for i in range(1, len(sys.argv)):
+        filename = sys.argv[i]
+        with open(filename) as file:
+            for j, line in enumerate(file):
+                if needHeader:
+                    print(line[:-1] + ",\"filename\"\n")
+                    needHeader = False
+                if j == 0 or line == '\n':
+                    continue
+                print(line[:-1] + f",\"{filename}\"\n")
 
 
-if name == '__main__':
+if __name__ == '__main__':
     main()
